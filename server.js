006c94632be6a71017012fd8c4fff942c31fb616
Biu-Parse <b.parseghian1991@gmail.com>
2025-07-08 00:38:31 -0700

    Fixed some errors, check auth and DB and create first user

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -9,7 +8,7 @@ const session = require('express-session');
 
 const authController = require('./controllers/auth.js');
 
-const port = process.env.PORT ? process.env.PORT : '4000';
+const port = process.env.PORT ? process.env.PORT : '3000';
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -19,7 +18,7 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -32,14 +31,6 @@ app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
   });
-});
-
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
 });
 
 app.use('/auth', authController);
