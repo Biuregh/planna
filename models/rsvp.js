ce514e8289d33951920a7c44c46316fae198933d
Biu-Parse <b.parseghian1991@gmail.com>
2025-07-07 20:56:06 -0700

    Create RSVP model

---
+++ models/rsvp.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const rsvpSchema = new mongoose.Schema({
+    guestId: {
+        type: mongoose.Schema.types.ObjectId,
+        ref: Guest,
+        required: true
+    },
+    attending: {
+        type: String,
+        enum: ["yes", "no", "pending"],
+        default: "pending",
+        required: true
+    },
+    mealChoice: {
+        type: String
+    }
+}, { timestamps: true });
+
+const RSVP = mongoose.model(RSVP, rsvpSchema);
+module.exports = RSVP;
